9b71f85f3b2d7e3a05910854d88da47c9d7ff988
baptiste perri <baptiste.perri7@etu.univ-lorraine.fr>
2025-12-19 06:49:35 +0100

    Premier commit : initialisation boilerplate

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Admin
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setFirstname('Admin');
+        $admin->setLastname('System');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, 'password'));
+        $manager->persist($admin);
+
+        // Manager
+        $managerUser = new User();
+        $managerUser->setEmail('manager@example.com');
+        $managerUser->setFirstname('Manager');
+        $managerUser->setLastname('System');
+        $managerUser->setRoles(['ROLE_MANAGER']);
+        $managerUser->setPassword($this->userPasswordHasher->hashPassword($managerUser, 'password'));
+        $manager->persist($managerUser);
+
+        // User
+        $user = new User();
+        $user->setEmail('user@example.com');
+        $user->setFirstname('John');
+        $user->setLastname('Doe');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
